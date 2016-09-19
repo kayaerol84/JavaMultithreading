17ea6b71ed4d91c1eb513edfed50b08144fe7ed9
Erol Kaya <kayaerol84@gmail.com>
2016-09-19 08:39:06 +0200

    Terminating threads

---
+++ TerminatingThreadSecondWay.java
@@ -0,0 +1,40 @@
+package threads.terminate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import threads.common.LoopTaskD;
+import threads.common.LoopTaskE;
+import threads.common.LoopTaskF;
+
+public class TerminatingThreadSecondWay {
+
+	public static void main(String[] args) throws InterruptedException {
+		String currentThreadName = Thread.currentThread().getName();
+		System.out.println("[" + currentThreadName + "] Main thread started");
+
+		LoopTaskF task1 = new LoopTaskF();
+		LoopTaskF task2 = new LoopTaskF();
+		LoopTaskF task3 = new LoopTaskF();
+		
+		Thread t1= new Thread(task1, "CoolThread-1");
+		t1.start();
+		Thread t2= new Thread(task2, "CoolThread-2");
+		t2.start();
+		Thread t3= new Thread(task3, "CoolThread-3");
+		t3.start();
+		TimeUnit.MILLISECONDS.sleep(3000);
+		
+		System.out.println("[" + currentThreadName + "] interrupting " + t1.getName() + "........");
+		t1.interrupt();
+		
+		System.out.println("[" + currentThreadName + "] interrupting " + t2.getName() + "........");
+		t2.interrupt();
+		
+		System.out.println("[" + currentThreadName + "] interrupting " + t3.getName() + "........");
+		t3.interrupt();
+		
+		System.out.println("[" + currentThreadName + "] Main thread ends");
+	}
+}
